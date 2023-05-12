7485ce637a09af105171c2185af38e0c2595ce81
胡明娣 <74650367+mendy-2013@users.noreply.github.com>
2023-05-12 19:35:42 +0800

    Add files via upload

---
+++ utils/utils_fit.py
@@ -0,0 +1,126 @@
+import os
+
+import torch
+from tqdm import tqdm
+
+from utils.utils import get_lr
+import torch.nn as nn
+loss_MSE = torch.nn.MSELoss(reduction='mean')
+def fit_one_epoch(model_train, model, focal_loss, loss_history, eval_callback, optimizer, epoch, epoch_step, epoch_step_val, gen, gen_val, Epoch, cuda, fp16, scaler, save_period, save_dir, local_rank=0):
+    loss        = 0
+    val_loss    = 0
+    loss_2 = 0
+
+    if local_rank == 0:
+        print('Start Train')
+        pbar = tqdm(total=epoch_step,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
+    model_train.train()
+    for iteration, batch in enumerate(gen):
+        if iteration >= epoch_step:
+            break
+        images, targets,gt_images = batch[0], batch[1],batch[2]
+        with torch.no_grad():
+            if cuda:
+                images  = images.cuda(local_rank)
+                gt_images = gt_images.cuda(local_rank)
+                targets = [ann.cuda(local_rank) for ann in targets]
+        #----------------------#
+        #   清零梯度
+        #----------------------#
+        optimizer.zero_grad()
+        if not fp16:
+            #-------------------#
+            #   获得预测结果
+            #-------------------#
+            _, regression, classification, anchors,unet_out = model_train(images)
+            #-------------------#
+            #   计算损失
+            #-------------------#
+            loss_value, _, _ = focal_loss(classification, regression, anchors, targets, cuda = cuda)
+
+            loss_value.backward()
+            torch.nn.utils.clip_grad_norm_(model_train.parameters(), 1e-1)
+            optimizer.step()
+        else:
+            from torch.cuda.amp import autocast
+            with autocast():
+                #-------------------#
+                #   获得预测结果
+                #-------------------#
+                _, regression, classification, anchors,u_net = model_train(images)
+                #-------------------#
+                #   计算损失
+                #-------------------#
+                loss_L2 = loss_MSE(u_net,gt_images)
+                loss_value, _, _ = focal_loss(classification, regression, anchors, targets, cuda = cuda)
+                loss_value = loss_value + 0.5*loss_L2
+            #----------------------#
+            #   反向传播
+            #----------------------#
+            scaler.scale(loss_value).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            
+        loss += loss_value.item()
+        loss_2 += loss_L2.item()
+        
+        if local_rank == 0:
+            pbar.set_postfix(**{'loss'  : loss / (iteration + 1),"loss_L2":loss_2/(iteration + 1),'lr'    : get_lr(optimizer)})
+            pbar.update(1)
+
+    if local_rank == 0:
+        pbar.close()
+        print('Finish Train')
+        print('Start Validation')
+        pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
+
+    model_train.eval()
+    for iteration, batch in enumerate(gen_val):
+        if iteration >= epoch_step_val:
+            break
+        images, targets,gt_val_image = batch[0], batch[1],batch[2]
+        with torch.no_grad():
+            if cuda:
+                images  = images.cuda(local_rank)
+                targets = [ann.cuda(local_rank) for ann in targets]
+                gt_val_image = gt_val_image.cuda(local_rank)
+            #----------------------#
+            #   清零梯度
+            #----------------------#
+            optimizer.zero_grad()
+            #-------------------#
+            #   获得预测结果
+            #-------------------#
+            _, regression, classification, anchors,unet_val_image = model_train(images)
+            #-------------------#
+            #   计算损失
+            #-------------------#\
+            loss_L2 = loss_MSE(u_net,gt_val_image)
+            loss_value, _, _ = focal_loss(classification, regression, anchors, targets, cuda = cuda)
+            loss_value = loss_value + 0.5*loss_L2
+            
+
+        val_loss += loss_value.item()
+        if local_rank == 0:
+            pbar.set_postfix(**{'val_loss': val_loss / (iteration + 1)})
+            pbar.update(1)
+
+    if local_rank == 0:
+        pbar.close()
+        print('Finish Validation')
+        loss_history.append_loss(epoch + 1, loss / epoch_step, val_loss / epoch_step_val)
+        eval_callback.on_epoch_end(epoch + 1, model_train)
+        print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
+        print('Total Loss: %.3f || Val Loss: %.3f ' % (loss / epoch_step, val_loss / epoch_step_val))
+        
+        #-----------------------------------------------#
+        #   保存权值
+        #-----------------------------------------------#
+        if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
+            torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, loss / epoch_step, val_loss / epoch_step_val)))
+            
+        if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
+            print('Save best model to best_epoch_weights.pth')
+            torch.save(model.state_dict(), os.path.join(save_dir, "best_epoch_weights.pth"))
+            
+        torch.save(model.state_dict(), os.path.join(save_dir, "last_epoch_weights.pth"))
